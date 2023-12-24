5e5cf05e79f01b0a1c53a7973dee463133335814
Göktuğ Ceyhan <goktugceyhan@gmail.com>
2023-12-25 00:35:52 +0300

    unit test fix

--- tests/getFixtures.test.ts
+++ tests/getFixtures.test.ts
@@ -1,53 +1,50 @@
 import sinon from 'sinon'
-import chai from 'chai'
-import chaiHttp from 'chai-http'
-import { app } from '../index'
-import * as redisModule from '../src/database/redis'
+import { expect } from 'chai'
+import { FixtureModel } from '../src/fixtures/model'
 
-const expect = chai.expect
-chai.use(chaiHttp)
-
-describe('getFixtures', () => {
-  let redisGetStub: sinon.SinonStub
+describe('Fixture Model', () => {
+  let findStub: sinon.SinonStub
 
   beforeEach(() => {
-    redisGetStub = sinon.stub(redisModule, 'redisClient').resolves({
-      get: sinon.stub().resolves(null)
-    })
+    findStub = sinon.stub(FixtureModel, 'find').resolves([
+      {
+        _id: '65888d3abd1d8ff75433481d',
+        season: '2017-2018',
+        league: 'Premier League',
+        date: '11/08/2017',
+        homeTeam: 'Arsenal',
+        awayTeam: 'Leicester',
+        homeGoals: 4,
+        awayGoals: 3,
+        Referee: 'M Dean',
+        __v: 0
+      }
+    ])
   })
 
   afterEach(() => {
     sinon.restore()
   })
 
-  it('should retrieve fixtures from Redis cache', (done) => {
-    const mockData = [
+  it('should retrieve fixtures correctly', async () => {
+    const fixtures = await FixtureModel.find({ league: 'Premier League', season: '2017-2018' })
+    expect(fixtures).to.deep.equal([
       {
-        Referee: 'C Pawson',
-        __v: 0,
-        _id: '65864880e361df4b10ff2fc7',
-        awayGoals: 0,
-        awayTeam: 'Newcastle',
-        date: '18/08/2018',
-        homeGoals: 0,
-        homeTeam: 'Cardiff',
+        _id: '65888d3abd1d8ff75433481d',
+        season: '2017-2018',
         league: 'Premier League',
-        season: '2018-2019'
+        date: '11/08/2017',
+        homeTeam: 'Arsenal',
+        awayTeam: 'Leicester',
+        homeGoals: 4,
+        awayGoals: 3,
+        Referee: 'M Dean',
+        __v: 0
       }
-    ]
-
-    // Redis returns a stringified version of the mock data
-    redisGetStub.resolves(JSON.stringify(mockData))
-
-    chai
-      .request(app)
-      .get('/fixtures?league=Premier League&season=2019-2020')
-      .end((err, res) => {
-        expect(res).to.have.status(200)
-        expect(res.body.source).to.equal('redis-cache')
-        expect(res.body.data).to.deep.equal(mockData)
-        done()
-        process.exit(0)
-      })
+    ])
+    expect(findStub.calledOnce).to.be.true
+  })
+  after(() => {
+    process.exit()
   })
 })
